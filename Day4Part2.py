150f2b3fbd699d3ee629f92e45029e24714c6568
Haaffiiizzz <dadaabdulhafiz0306@gmail.com>
2025-12-08 03:30:56 -0600

    Day 4

---
+++ Day4Part2.py
@@ -0,0 +1,71 @@
+grid = []
+
+with open("Day4Input.txt", "r") as file:
+    lines = file.readlines()
+    for line in lines:
+        grid.append(list(line.strip()))
+
+n, m = len(grid), len(grid[0])
+res = 0
+
+
+while True:
+    changed = False
+    for i in range(n):
+        for j in range(m):
+            curr = grid[i][j]
+            count = 0
+
+            if curr == "@":
+                
+                if j > 0: # we can go left
+                    if grid[i][j-1] == "@": # left
+                        count += 1
+                    
+                    if i > 0: # we can go up left
+                        if grid[i-1][j-1] == "@":
+                            count += 1
+
+                    if i < n -1: # we can do down left
+                        if grid[i+1][j-1] == "@":
+                            count += 1
+
+                if j < m - 1: # we can go right
+                    if grid[i][j+1] == "@": #right
+                        count += 1
+                    
+                    if i > 0: #up right
+                        if grid[i-1][j+1] == "@":
+                            count += 1
+
+                    if i < n - 1: #down right
+                        if grid[i+1][j+1] == "@":
+                            count += 1
+                
+                if i > 0: #up
+                    if grid[i-1][j] == "@":
+                        count += 1
+                
+                if i < n - 1: #down
+                    if grid[i + 1][j] == "@":
+                        count  += 1 
+            else:
+                count = 8
+
+            if count < 4:
+                res += 1
+                grid[i][j] = "."
+                changed = True
+                break
+        if changed:
+            break
+    if not changed:
+        break
+
+print(res)
+
+                
+
+
+
+            
